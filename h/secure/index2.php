78b454f3227e67823e0f1feb9044f7e44293750d
paranya0706 <66010914051@msu.ac.th>
2026-02-04 16:51:43 +0700

    a

--- h/secure/index2.php
+++ h/secure/index2.php
@@ -1,11 +1,4 @@
-<?php
-    session_start();
-    if(empty($_SESSION['aid'])) {
-    echo "Access Denied" ;
-	echo "<meta http-equiv='refresh' content='3; url=index.php'>" 
-    exit;
-    }   
-?>
+
 <!doctype html>
 <html>
 <head>

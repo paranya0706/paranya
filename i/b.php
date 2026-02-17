146d71e9f6d8ca1319ba15e7a1d9ff53f55d88ff
paranya0706 <66010914051@msu.ac.th>
2026-02-17 20:28:30 +0700

    a

--- i/b.php
+++ i/b.php
@@ -2,55 +2,70 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>ปรัณญา ศรีบุญเพ็ง(หมวย)</title>
+<title>ปรัณญา ศรีบุญเพ็ง (หมวย)</title>
 </head>
 
 <body>
-<h1>งาน i -- ปรัณญา ศรีบุญเพ็ง(หมวย)</h1>
+<h1>งาน i -- ปรัณญา ศรีบุญเพ็ง (หมวย)</h1>
 
-<form method="post" action="">
-    ชื่อภาค <input type="text" name="rname" autofocus required>
-    <button type="submit" name="Submit">บันทึก</button>
-    </form><br><br>
+<form method="post" action="" encctype="multipart/form-data">
+    ชื่อจังหวัด <input type="text" name="pname" autofocus required> <br>
+    รูปภาพ<input type="file" name="pimaage" reguired> <br>
     
+    ภาค
+    <select name="rid">
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM provice";
+$rs = mysqli_query($conn,$sql3);
+while($data = mysqli_fetch_array($rs3)){
+?>
+         <option value="<?php echo $data3['r_id']; ?>"><?php echo $data3['r_name']; ?></option>
+<?php } ?>     
+     </select>
+     <br>
+     
+    <button type="submit" name="Submit"> บันทึก </button>
+</form><br><br>
+
 <?php
 if(isset($_POST['Submit'])){
     include_once("connectdb.php");
-    $rname = $_POST['rname'];
-    $sql = "INSERT INTO regions (r_id, r_name) VALUES (NULL,'{$rame}')";
+    $rname = $_POST['pname'];
+	$ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
+	$rid = $_POST['rid'];
+	
+    $sql2 = "INSERT INTO  `provinces` VALUES (NULL, '{$pname}','{ext}','{rname}')";
     mysqli_query($conn,$sql2) or die ("เพิ่มข้อมูลไม่ได้");
+	$pid = mysqli_insert_id($conn);
+	copy($_FILES['pimage']['tmp_name'], "image/".$pid.".");
 }
-
-<?php
-include_once("connectdb.php");
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn,$sql);
-while($data = mysqli_fetch_array($rs)){
-    ehco $data['r_id'] . "<br>";
-    ehco $data['r_name'] . "<hr>";
-    }
-mysqli_close($conn);
 ?>
 
 <table border="1">
     <tr>
-        <th>รหัสภาค</th>
-        <th>ชื่อภาค</th>
+        <th>รหัสจังหวัด</th>
+        <th>ชื่อจังหวัด</th>
+        <th>รูป</th>
         <th>ลบ</th>
     </tr>
 <?php
-include_once("connectdb.php")
-$sql = "SELECT * FROM regions";
+include_once("connectdb.php");
+$sql = "SELECT * FROM provice";
 $rs = mysqli_query($conn,$sql);
 while($data = mysqli_fetch_array($rs)){
 ?>
     <tr>
-        <td><?php ehco $data['r_id'] ; ?></td>
-        <td><?php ehco $data['r_name'] ; ?></td>
-        <td width="80" align="center"><img src="../img/delete.jpg" width="20"></td>
+        <td><?php echo $data['p_id']; ?></td>
+        <td><?php echo $data['p_name']; ?></td>
+        <td><img src="images/<?php echo $data['p_id']; ?>.<?php echo $data['p_name']; ?>jpg" width="140"></td>
+        <td width="80" align="center"><a href="delete_regions.php?id=<?php echo $data['p_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg" width="20"></a></td>
     </tr>
-    <?php} ?>
+<?php } ?>
 </table>
 
+<?php
+mysqli_close($conn);
+?>
 </body>
 </html>

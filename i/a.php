aaf0604dfae89906888969aa9f61461db0b9d4fd
paranya0706 <66010914051@msu.ac.th>
2026-02-17 18:49:40 +0700

    a

---
+++ i/a.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ปรัณญา ศรีบุญเพ็ง(หมวย)</title>
+</head>
+
+<body>
+<h1>งาน i -- ปรัณญา ศรีบุญเพ็ง(หมวย)</h1>
+
+<form method="post" action="">
+	ชื่อภาค <input type="text" name="rname" autofocus required>
+    <button type="submit" name="Submit">บันทึก</button>
+    </form><br><br>
+
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM `regions`";
+$rs = mysqli_query($conn,$sql);
+while($data = mysqli_fetch_array($rs)){
+	ehco $data['r_id'] . "<br>";
+    ehco $data['r_name'] . "<hr>";
+    }
+mysqli_close($conn);
+?>
+
+<table border="1">
+	<tr>
+    	<th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+        <th>ลบ</th>
+    </tr>
+<?php
+include_once("connectdb.php")
+$sql = "SELECT * FROM `regions`";
+$rs = mysqli_query($conn,$sql);
+while($data = mysqli_fetch_array($rs)){
+?>
+	<tr>
+    	<td><?php ehco $data['r_id'] ; ?></td>
+        <td><?php ehco $data['r_name'] ; ?></td>
+        <td width="80" align="center"><img src="../img/delete.jpg" width="20"></td>
+    </tr>
+    <?php} ?>
+</table>
+
+</body>
+</html>

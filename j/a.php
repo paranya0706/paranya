4e97680741f61d71faaea8534dd83c3fab88fba1
paranya0706 <66010914051@msu.ac.th>
2026-02-18 13:38:07 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ปรัณญา ศรีบุญเพ็ง (หมวย)</title>
+</head>
+<h1>งาน j -- 66010914051 ปรัณญา ศรีบุญเพ็ง</h1>
+
+<img src="1.jpg"width=400>
+
+<body>
+</body>
+</html>

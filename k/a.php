0bc0aed1eb5af284adc44616cadfa9a480e20dfb
paranya0706 <66010914051@msu.ac.th>
2026-02-18 17:07:47 +0700

    a

--- k/a.php
+++ k/a.php
@@ -3,30 +3,59 @@
 <head>
 <meta charset="utf-8">
 <title>ปรัณญา ศรีบุญเพ็ง (หมวย)</title>
+
+<style>
+    input[type="radio"] {
+        display: none;
+    }
+
+    .btn {
+        padding: 8px 15px;
+        border: 1px solid #0d6efd;
+        background-color: white;
+        color: #0d6efd;
+        cursor: pointer;
+        margin-right: 5px;
+    }
+
+    .btn:hover {
+        background-color: #0d6efd;
+        color: white;
+    }
+
+    img {
+        display: none;
+        margin-top: 20px;
+        width: 300px;
+    }
+
+    #img1:checked ~ .photo1 {
+        display: block;
+    }
+
+    #img2:checked ~ .photo2 {
+        display: block;
+    }
+</style>
+
 </head>
 
 <body>
 
 <h1>งาน k -- 66010914051 ปรัณญา ศรีบุญเพ็ง</h1>
 
+<!-- ปุ่ม -->
+<input type="radio" name="photo" id="img1">
+<label for="img1" class="btn">รูปที่ 1</label>
 
-<details>
-    <summary style="cursor:pointer; font-size:18px; color:blue;">
-        กดดูรูปที่ 1
-    </summary>
-    <br>
-    <img src="12.jpg" width="300">
-</details>
+<input type="radio" name="photo" id="img2">
+<label for="img2" class="btn">รูปที่ 2</label>
 
 <br>
 
-<details>
-    <summary style="cursor:pointer; font-size:18px; color:green;">
-        กดดูรูปที่ 2
-    </summary>
-    <br>
-    <img src="11.jpg" width="300">
-</details>
+<!-- รูปภาพ -->
+<img src="12.jpg" class="photo1">
+<img src="11.jpg" class="photo2">
 
 </body>
 </html>

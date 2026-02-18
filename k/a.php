455f18f780b3951e9e7e267da8e787676f00c41b
paranya0706 <66010914051@msu.ac.th>
2026-02-18 17:04:24 +0700

    a

--- k/a.php
+++ k/a.php
@@ -4,15 +4,29 @@
 <meta charset="utf-8">
 <title>ปรัณญา ศรีบุญเพ็ง (หมวย)</title>
 </head>
-<h1>งาน k -- 66010914051 ปรัณญา ศรีบุญเพ็ง</h1>
-
-<button type="button" onclick="showImage()">แสดงรูป</button>
-
-<br><br>
-
-<img id="11.jpg" src="" width="300" style="display:none;">
-
 
 <body>
+
+<h1>งาน k -- 66010914051 ปรัณญา ศรีบุญเพ็ง</h1>
+
+
+<details>
+    <summary style="cursor:pointer; font-size:18px; color:blue;">
+        กดดูรูปที่ 1
+    </summary>
+    <br>
+    <img src="12.jpg" width="300">
+</details>
+
+<br>
+
+<details>
+    <summary style="cursor:pointer; font-size:18px; color:green;">
+        กดดูรูปที่ 2
+    </summary>
+    <br>
+    <img src="11.jpg" width="300">
+</details>
+
 </body>
 </html>

8f5661d9ac571aaa7605d3d27efbfd3420716063
paranya0706 <66010914051@msu.ac.th>
2026-03-04 13:07:37 +0700

    a

--- i/delete_regions.php
+++ i/delete_regions.php
@@ -1,13 +1,13 @@
 <meta charset="utf-8">
 <?php
-include_once("connectdb.php");
+include_once(connectdb.php);
 
-$id = $_GET['id'];
-$sql = "DELETE FROM regions WHEHE r_id='{id}'";
-mysqli_query($conn.$sql) or die ("ลบข้อมูลไม่ได้");
+$id = $_GET['id']; // รับค่า id จาก URL
+$sql = "DELETE FROM regions WHERE r_id='{$id}'"; // สร้างคำสั่ง SQL เพื่อลบข้อมูล
+mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
 
-echo "<script>";
-echo "window.location='a.php';";
-echo "</scrip>";
+echo "<script>" ;
+echo "window.location='a.php';" ;
+echo "</script>" ;
 
 ?>

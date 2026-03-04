c602000f94b8f2c795bcbd7a094395a29ff14514
paranya0706 <66010914051@msu.ac.th>
2026-03-04 13:01:18 +0700

    a

--- i/delete_province.php
+++ i/delete_province.php
@@ -1,17 +1,18 @@
 <meta charset="utf-8">
 <?php
-include_once(connectdb.php);
+include_once(connectdb.php); // เรียกไฟล์เชื่อมต่อฐานข้อมูล
 
-$id = $_GET['id'];
-$ext = $_GET['ext'];
+$id = $_GET['id']; // รับค่าจาก URL (GET)
+$ext = $_GET['ext']; // รับค่าจาก URL (GET)
 
-$sql = "DELETE FROM provices WHERE p_id='{$id}'";
-mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
+$sql = "DELETE FROM provices WHERE p_id='{$id}'"; // คำสั่ง SQL ลบข้อมูล
+// เป็นคำสั่ง ลบข้อมูลในตาราง provices โดยลบแถวที่ p_id ตรงกับ $id
+mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้"); // รันคำสั่ง SQL
 
-unlink("images/".$id.".".$ext);
+unlink("images/".$id.".".$ext); // ลบไฟล์รูปภาพ
 
 echo "<script>" ;
-echo "window.location='b.php';" ;
+echo "window.location='b.php';" ; // เปลี่ยนหน้าไปยัง b.php
 echo "</script>" ;
 
 ?>
